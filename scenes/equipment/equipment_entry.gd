9e86b332e1f64fba3d4afe08da65d299f5846ae8
Ingvar Hahn Kristensen <ingvar@hahnkristensen.dk>
2025-08-13 16:42:17 +0200

    ability for equipment to have cost and be unlockable

--- scenes/equipment/equipment_entry.gd
+++ scenes/equipment/equipment_entry.gd
@@ -1,10 +1,11 @@
 class_name EquipmentEntry
 extends Resource
 
-## The scene to be spawned.
-@export var scene: PackedScene
 @export var name: String
-@export var unlocked: bool
 
-## Maybe useful in knowing when to unlock this equipment
-@export var value: int
+## The scene to be spawned
+@export var scene: PackedScene
+
+## Cost to unlock the equipment
+@export var cost: int
+var unlocked: bool = false
