2518843c4626987915080c2b43308ba08dfa6330
Ingvar Hahn Kristensen <ingvar@hahnkristensen.dk>
2025-08-14 20:37:49 +0200

    added ability for arm to stretch past struggle extension

--- scenes/player/hand/hand.gd
+++ scenes/player/hand/hand.gd
@@ -1,6 +1,10 @@
 extends Node2D
 
-@export var _max_vertical_extension: float = 100
+@export var _max_vertical_arm_extension: float = 100
+@export var _struggle_extension: float = 70
+@onready var _logistic_constant: float = -log((_max_vertical_arm_extension/\
+_struggle_extension)-1)/(_struggle_extension-_max_vertical_arm_extension/2)
+
 @export var _inventory: InventoryUI
 @export var _inventory_component: InventoryComponent
 
@@ -11,15 +15,29 @@ extends Node2D
 @export var equipment_state: EquipmentState
 
 func _ready() -> void:
+	assert(!is_nan(_logistic_constant), "Invalid values for arm extension")
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	
 	idle_state.initialize(_inventory) # Fix for weird missing reference bug
 	pick_up_state.initialise(_inventory_component, _inventory)
 	equipment_state.initialise(_inventory_component, _inventory)
 	smack_state.initialise(_inventory)
 
+## Converts regular coordinates to godot coordinates and vice versa
+func _coords_convert(in_cords: float) -> float:
+	return get_window().content_scale_size.y - in_cords
+
 func _process(_delta: float) -> void:
 	var mouse_pos = get_global_mouse_position()
-	var extension_from_top = get_window().content_scale_size.y-_max_vertical_extension
-	global_position = Vector2(mouse_pos.x, max(extension_from_top, mouse_pos.y))
+	var y_pos = _coords_convert(mouse_pos.y)
+	
+	## Arm is limited by a logistic function if extended past the struggle bound
+	if _coords_convert(mouse_pos.y) > _struggle_extension:		
+		var variable = _coords_convert(mouse_pos.y)
+		y_pos = _max_vertical_arm_extension/(1+exp(-_logistic_constant\
+		*(variable-_max_vertical_arm_extension/2)))
+		
+	global_position = Vector2(mouse_pos.x, _coords_convert(y_pos))
 	
 #func _input(event: InputEvent) -> void:
 	#handle_equipment_change(event)
