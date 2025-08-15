46214b7d981c21ea36f8fe886b3dbcc7d6c2d925
Mattis Kristensen <mattismoel@gmail.com>
2025-08-15 16:37:44 +0200

    add cursor class

---
+++ scenes/ui/cursor.gd
@@ -0,0 +1,19 @@
+class_name Cursor
+extends Control
+
+@export var _texture_rect: TextureRect
+@export var _cursor_sprite_dict: Dictionary[Input.CursorShape, CursorEntry] = {}
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
+	pass
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		global_position = event.global_position
+
+	var cursor_shape := Input.get_current_cursor_shape()
+
+	var entry := _cursor_sprite_dict[cursor_shape]
+	_texture_rect.texture = entry.texture
+	_texture_rect.position = entry.offset
---
+++ scenes/ui/cursor_entry.gd
@@ -0,0 +1,5 @@
+class_name CursorEntry
+extends Resource
+
+@export var texture: Texture2D
+@export var offset: Vector2
