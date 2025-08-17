e7800c8a8cd94e0933b759aba35d929d65f9ee24
Mattis Kristensen <mattismoel@gmail.com>
2025-08-17 17:59:53 +0200

    refactor cutlery

--- scenes/equipment/cutlery/idle_state.gd
+++ scenes/equipment/cutlery/idle_state.gd
@@ -6,15 +6,18 @@ extends State
 
 func input(event: InputEvent) -> void:
 	super(event)
+
 	if !event.is_action_pressed("interact"): return
 
 	for area in _pickup_area.get_overlapping_areas():
-		if area is not Target: continue
+		if area is not Target: 
+			continue
+
 		var target := area as Target
-		var consumable := target.consumable
+		var consumable := (area as Target).consumable
 
 		if target.is_empty(): continue
-		if !_is_allowed_consumable(consumable): continue
+		if !_is_allowed_consumable(consumable): return
 
 		_holding_state.set_consumable(consumable)
 		changed_state.emit(_holding_state)
@@ -22,4 +25,5 @@ func input(event: InputEvent) -> void:
 ## Returns whether or not the consumable is allowed to be picked up by the 
 ## spoon.
 func _is_allowed_consumable(consumable: ConsumableEntry) -> bool:
-	return _allowed_consumables.has(consumable)
+	var allowed := _allowed_consumables.has(consumable)
+	return allowed
