80125412f8331de545239ee21a409f67eb141c37
Ingvar Hahn Kristensen <ingvar@hahnkristensen.dk>
2025-08-17 18:20:24 +0200

    fix equipment not starting at mouse pos when bought

--- scenes/equipment/small_zapper/idle_state.gd
+++ scenes/equipment/small_zapper/idle_state.gd
@@ -1,8 +1,11 @@
 extends State
 
 @export var _zap_state: ZapState
+@export var _struggle_component: StruggleComponent
 
 func enter() -> void:
+	var mouse_pos := get_viewport().get_mouse_position()
+	global_position = _struggle_component.get_struggling_mouse_pos(mouse_pos)
 	super()
 	show()
 
--- scenes/equipment/spray/idle_state.gd
+++ scenes/equipment/spray/idle_state.gd
@@ -2,8 +2,11 @@ extends State
 
 @export var _spray_state: SprayState
 @export var _animation_player: AnimationPlayer
+@export var _struggle_component: StruggleComponent
 
 func enter() -> void:
+	var mouse_pos := get_viewport().get_mouse_position()
+	global_position = _struggle_component.get_struggling_mouse_pos(mouse_pos)
 	super()
 	show()
 	_animation_player.play("idle")
