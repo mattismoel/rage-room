6f04fd4e714661eb590beb87dcd9137f01c58c19
Ingvar Hahn Kristensen <ingvar@hahnkristensen.dk>
2025-08-17 19:40:13 +0200

    added spray can sound effect

--- scenes/equipment/spray/spray_state.gd
+++ scenes/equipment/spray/spray_state.gd
@@ -6,19 +6,25 @@ extends State
 @export_category("References")
 @export var _idle_state: State
 @export var _hit_area: Area2D
+
 @export var _animation_player: AnimationPlayer
+@export var _audio_player: AudioStreamPlayer2D
+@export var _pitch_variance: float = 0.05
 
 func enter() -> void:
 	super()
-	show()
 
 	_animation_player.play("spray")
 	await _animation_player.animation_finished
 	changed_state.emit(_idle_state)
 
+func play_sound() -> void:
+	_audio_player.pitch_scale = 1.0 + randf_range(-_pitch_variance * 0.5, _pitch_variance * 0.5)
+	_audio_player.play()
+
 func exit() -> void:
 	super()
-	hide()
+	_audio_player.stop()
 	
 func _spray_intersecting_insects() -> void:
 	var overlapping_areas: Array[Area2D] = _hit_area.get_overlapping_areas()
